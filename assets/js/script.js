4af6c1413939cb090a20a11382e4f99378ae1aae
CardonThibeau <thibeau.cardon@student.howest.be>
2018-08-15 21:30:06 +0200

    fix path service worker

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //------------------------------------------- Service Worker -------------------------------------------
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js', { scope: '/' }).then(function(reg) {
+    navigator.serviceWorker.register('/Dibijoux/sw.js', { scope: '/Dibijoux/' }).then(function(reg) {
 
         if(reg.installing) {
             console.log('Service worker installing');
