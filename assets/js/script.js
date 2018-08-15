dee0aa9e8130906c47e0603fcf6221cc4033c627
CardonThibeau <thibeau.cardon@student.howest.be>
2018-08-15 21:32:38 +0200

    another fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //------------------------------------------- Service Worker -------------------------------------------
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/Dibijoux/sw.js', { scope: '/Dibijoux/' }).then(function(reg) {
+    navigator.serviceWorker.register('/Dibijoux/sw.js', { scope: '/Dibijoux' }).then(function(reg) {
 
         if(reg.installing) {
             console.log('Service worker installing');
